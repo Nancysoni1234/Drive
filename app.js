e24bdbe38dee8c7bca7dd6dd4d4c55b3e8cc4f41
Nancy Soni <ns1915@srmist.edu.in>
2025-05-12 22:23:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
-const express = require('express');
-const app = express();
-const userRouter = require('./routes/user_routes');
+const express = require('express')
+const router = express.Router();
 
-app.set('view engine','ejs')
+const bcrypt = require('bcrypt');
 
-app.use('/user',userRouter);
+const { body,validationResult } = require('express-validator');
 
-app.listen(3000);
+const userModel = require('../models/userModel');
+
+
+router.get('/',(req,res)=>{
+    res.render('index');
+})
+
+router.post('/index', 
+
+    body('Email').trim().isEmail().isLength({min:6}),
+    body('Username').trim().isLength({min:3}),
+    body('Passward').trim().isLength({min:4}),
+
+      async(req,res)=>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({
+                errors:errors.array(),
+                message:'Invalid Data'
+            })
+        }
+        const {Username, Email, Passward} = req.body
+
+        const hashPassward = await bcrypt.hash(Passward,10);      //npm i bcrypt 
+
+        const newUser = await userModel.create({
+            Username,
+            Email,
+            Passward:hashPassward
+        })
+
+        res.json(newUser);
+
+
+    //    res.send("User registered")
+})
+
+module.exports = router;
