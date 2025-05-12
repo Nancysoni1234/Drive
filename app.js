93fbaf9e0c6c191c99b79621bbe45c87f0c692a8
Nancy Soni <ns1915@srmist.edu.in>
2025-05-12 22:23:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,45 +1,19 @@
-const express = require('express')
-const router = express.Router();
+const express = require('express');
+const app = express();
+const userRouter = require('./routes/user_routes');
 
-const bcrypt = require('bcrypt');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const { body,validationResult } = require('express-validator');
+const connectToDB = require('./config/db');
+connectToDB();
 
-const userModel = require('../models/userModel');
+//to see the console value in terminal we need these middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
+app.set('view engine','ejs')
 
-router.get('/',(req,res)=>{
-    res.render('index');
-})
+app.use('/user',userRouter);
 
-router.post('/index', 
-
-    body('Email').trim().isEmail().isLength({min:6}),
-    body('Username').trim().isLength({min:3}),
-    body('Passward').trim().isLength({min:4}),
-
-      async(req,res)=>{
-        const errors = validationResult(req);
-        if(!errors.isEmpty()){
-            return res.status(400).json({
-                errors:errors.array(),
-                message:'Invalid Data'
-            })
-        }
-        const {Username, Email, Passward} = req.body
-
-        const hashPassward = await bcrypt.hash(Passward,10);      //npm i bcrypt 
-
-        const newUser = await userModel.create({
-            Username,
-            Email,
-            Passward:hashPassward
-        })
-
-        res.json(newUser);
-
-
-    //    res.send("User registered")
-})
-
-module.exports = router;
+app.listen(3000);
